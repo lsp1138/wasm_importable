3bad0f143bebfebc80c1308be9c9e33e9c98342a
lsp1138 <larspittman@gmail.com>
2022-11-18 18:03:26 +0000

    added comments for the link statement in main

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,7 +1,7 @@
 // main module that imports an "extern" library exported
 // from the 'importable' library
 
-
+//#[link(wasm_import_module="importable")]
 extern "C" {
     fn add(x: i32, y: i32) -> i32;
 }
